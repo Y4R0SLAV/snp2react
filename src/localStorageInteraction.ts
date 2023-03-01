c1164ddaac9f5422c91eace1a317bd3d755b7ab3
Y4ROSLAV <jeros1337@gmail.com>
2023-03-01 21:56:50 +0300

    Add localStorage: interceptors

---
+++ src/localStorageInteraction.ts
@@ -0,0 +1,13 @@
+import { TodoItemType } from "./components/Content/ContentParts/ContentMain/TodoItem/TodoItem"
+
+export const getTodosLS = (): Array<TodoItemType> => {
+  const todos = localStorage.getItem('todos')
+  if (todos) {
+    return JSON.parse(todos)
+  }
+  return []
+}
+
+export const setTodosLS = (todoList: Array<TodoItemType> = []) => {
+  localStorage.setItem('todos', JSON.stringify(todoList))
+}
