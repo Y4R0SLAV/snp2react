9d49b564d8edde188e831082e782772505e6c39e
Y4ROSLAV <jeros1337@gmail.com>
2023-03-04 19:10:06 +0300

    Add TodoItem: editing logic

--- src/components/Content/ContentParts/ContentMain/TodoItem/TodoItem.tsx
+++ src/components/Content/ContentParts/ContentMain/TodoItem/TodoItem.tsx
@@ -1,61 +1,95 @@
 import {FC, useState} from 'react'
-import { useDispatch } from 'react-redux'
-import { changeTodo, removeTodo } from '../../../../../redux/reducers/todos'
-import s from "./TodoItem.module.css"
+import {useDispatch} from 'react-redux'
+import { changeCompletedTodo, changeTextTodo, removeTodo} from '../../../../../redux/reducers/todos'
+import s from './TodoItem.module.css'
+import classNames from 'classnames/bind'
 
+const TodoItemCheckbox: FC<{id: string; completed: boolean}> = ({id, completed}) => {
+	const dispatch = useDispatch()
 
-const customInput = () => {
-  return 
+	const onClickHandler = () => {
+		dispatch(changeCompletedTodo({id, completed: !completed}))
+	}
+
+	return (
+		<input
+			onClick={(e) => onClickHandler()}
+			className={s.todo__toggle}
+			type='checkbox'
+			defaultChecked={completed}
+		/>
+	)
 }
 
-export const TodoItem: FC<TodoItemType> = ({title, id, completed}) => {
-  const [editingText, setEditingText] = useState(title)
-  const [editing, setEditing] = useState(false)
+export const TodoItem: FC<TodoItemType & {hide: boolean}> = ({title, id, completed, hide}) => {
+	const [editingText, setEditingText] = useState(title)
+	const [editing, setEditing] = useState(false)
+	const cx = classNames.bind(s)
 
-  const dispatch = useDispatch()
+	const dispatch = useDispatch()
 
-  const change = () => {
-    dispatch(changeTodo({id, title: editingText, completed}))
-    setEditing(false)
-  }
+	const change = () => {
+		dispatch(changeTextTodo({id, title: editingText}))
+		setEditing(false)
+	}
 
-  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEditingText(e.currentTarget.value)
-  }
+	const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setEditingText(e.currentTarget.value)
+	}
 
-  const onClickHandler = (id: string) => {
-    dispatch(removeTodo(id))
-  }
+	const onClickHandler = (id: string) => {
+		dispatch(removeTodo(id))
+	}
 
-  const onDoubleClickHandler = () => { setEditing(true) }
+	const onDoubleClickHandler = () => {
+		setEditing(true)
+	}
 
-  const onKeyDownHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      e.preventDefault()
-      change()
-    }
-  }
+	const onKeyDownHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === 'Enter') {
+			e.preventDefault()
+			change()
+		}
+	}
 
-  return <li id={id} className={editing && s.editing}>
-    <div className={s.todo__item}>
+	return (
+		// у li класс completed если выполнен соответсвенно
+		<li
+			id={id}
+			className={cx({editing, completed, hide})}
+		>
+			<div className={s.todo__item}>
+				<TodoItemCheckbox
+					completed={completed}
+					id={id}
+				/>
 
-      <input className={s.todo__toggle} type="checkbox" defaultChecked={completed} />
+				<label
+					className={s.todo__text}
+					onDoubleClick={(e) => onDoubleClickHandler()}
+				>
+					{title}
+				</label>
+				<button
+					onClick={(e) => onClickHandler(id)}
+					className={s.todo__remove}
+				></button>
+			</div>
 
-      <label className={s.todo__text} onDoubleClick={e => onDoubleClickHandler()}>{title}</label>
-      <button onClick={e => onClickHandler(id)} className={s.todo__remove}></button>
-    </div>
-
-    <input  type="text" 
-            className={s.todo__edit} 
-            value={editingText} 
-            onChange={e => onChangeHandler(e)}
-            onBlur={e => change()}
-            onKeyDown={e => onKeyDownHandler(e)}></input>
-  </li>
+			<input
+				type='text'
+				className={s.todo__edit}
+				value={editingText}
+				onChange={(e) => onChangeHandler(e)}
+				onBlur={(e) => change()}
+				onKeyDown={(e) => onKeyDownHandler(e)}
+			></input>
+		</li>
+	)
 }
 
 export type TodoItemType = {
-  title: string,
-  id: string,
-  completed: boolean
+	title: string
+	id: string
+	completed: boolean
 }
