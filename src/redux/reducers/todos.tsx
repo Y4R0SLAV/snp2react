abf4d315431ee1a2dc91eb23ed34fc8fb1cca367
Y4ROSLAV <jeros1337@gmail.com>
2023-03-04 19:09:00 +0300

    Add redux: todos logic

--- src/redux/reducers/todos.tsx
+++ src/redux/reducers/todos.tsx
@@ -1,63 +1,117 @@
-import { createSlice } from '@reduxjs/toolkit'
-import type { PayloadAction } from '@reduxjs/toolkit'
-import { TodoItemType } from '../../components/Content/ContentParts/ContentMain/TodoItem/TodoItem'
-import { RootState } from '../store'
-import { stringify } from 'querystring'
+import {createSlice} from '@reduxjs/toolkit'
+import type {PayloadAction} from '@reduxjs/toolkit'
+import {TodoItemType} from '../../components/Content/ContentParts/ContentMain/TodoItem/TodoItem'
+import {RootState} from '../store'
+
+export const AllType = 'All'
+export const ActiveType = 'Active'
+export const CompletedType = 'Completed'
+
+export type FilterType = typeof AllType | typeof ActiveType | typeof CompletedType
 
 interface TodosState {
-  todos: Array<TodoItemType>,
-  count: number,
-  
+	todos: Array<TodoItemType>
+	count: number
+	activeCount: number
+	completedCount: number
+	todosFilter: FilterType
 }
 
 const initialState: TodosState = {
-  todos: [],
-  count: 0,
+	todos: [],
+	count: 0,
+	activeCount: 0,
+	completedCount: 0,
+	todosFilter: AllType,
 }
 
 export const todosSlice = createSlice({
-  name: 'todos',
-  initialState,
-  reducers: {
-    increment: (state) => {
-      state.count += 1
-    },
-    decrement: (state) => {
-      state.count -= 1
-    },
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.count += action.payload
-    },
-    addTodo: (state, action: PayloadAction<TodoItemType>) => {
-      state.todos.push(action.payload)
-    },
-    removeTodo: (state, action: PayloadAction<string>) => {
-      state.todos = [...state.todos.filter(todo => todo.id !== action.payload)]
-    },
-    initializeTodos: (state, action: PayloadAction<Array<TodoItemType>>) => {
-      state.todos = action.payload
-    },
-    changeTodo: (state, action: PayloadAction<TodoItemType>) => {
-      const newTodos = state.todos.map(todo => {
-        if (todo.id === action.payload.id) {
-          todo.title = action.payload.title
-          todo.completed = action.payload.completed
-        }
-        return todo
-      })
-      console.log(newTodos)
-      state.todos = newTodos
-    }
+	name: 'todos',
+	initialState,
+	reducers: {
+		addTodo: (state, action: PayloadAction<TodoItemType>) => {
+			state.todos.push(action.payload)
+			state.activeCount += 1
+		},
 
-  },
+		removeTodo: (state, action: PayloadAction<string>) => {
+			let removingTodo = state.todos.find((todo) => todo.id === action.payload)
+
+			state.todos = [...state.todos.filter((todo) => todo.id !== action.payload)]
+			state.count -= 1
+
+			if (removingTodo?.completed === false) {
+				state.activeCount -= 1
+			} else {
+				state.completedCount -= 1
+			}
+		},
+
+		initializeTodos: (state, action: PayloadAction<Array<TodoItemType>>) => {
+			state.todos = action.payload
+
+			let activeCount = action.payload.filter((item) => item.completed === false).length
+			let todosCount = action.payload.length
+
+			state.count = todosCount
+			state.activeCount = activeCount
+			state.completedCount = todosCount - activeCount
+			// не инициализирую фильтр, т.к. скорее всего, он стоит на All, иначе уже отдельно изменю
+		},
+
+		changeTextTodo: (state, action: PayloadAction<{id: string; title: string}>) => {
+			const newTodos = state.todos.map((todo) => {
+				if (todo.id === action.payload.id) {
+					todo.title = action.payload.title
+				}
+				return todo
+			})
+			state.todos = newTodos
+		},
+
+		changeCompletedTodo: (state, action: PayloadAction<{id: string; completed: boolean}>) => {
+			let activeCount = 1
+			// была активная задача, стала завершенной
+			if (action.payload.completed === true) {
+				activeCount = -1
+			}
+
+			state.activeCount += activeCount
+			state.completedCount -= activeCount
+
+			const newTodos = state.todos.map((todo) => {
+				if (todo.id === action.payload.id) {
+					todo.completed = action.payload.completed
+				}
+				return todo
+			})
+			state.todos = newTodos
+		},
+
+		clearCompletedTodos: (state) => {
+			const newTodos = state.todos.filter((todo) => todo.completed === false)
+			state.todos = newTodos
+		},
+
+		setFilter: (state, action: PayloadAction<FilterType>) => {
+			state.todosFilter = action.payload
+		},
+	},
 })
 
+export const {
+	addTodo,
+	removeTodo,
+	initializeTodos,
+	changeTextTodo,
+	changeCompletedTodo,
+	clearCompletedTodos,
+	setFilter,
+} = todosSlice.actions
 
-export const { increment, decrement, incrementByAmount, addTodo, removeTodo, initializeTodos, changeTodo} = todosSlice.actions
-
-// Other code such as selectors can use the imported `RootState` type
-export const selectCount = (state: RootState) => state.todos.count
+export const selectActiveTodosCount = (state: RootState) => state.todos.activeCount
+export const selectCompletedTodosCount = (state: RootState) => state.todos.completedCount
 export const selectTodos = (state: RootState) => state.todos.todos
-
+export const selectFilter = (state: RootState) => state.todos.todosFilter
 
 export default todosSlice.reducer
