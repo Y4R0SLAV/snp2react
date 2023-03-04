8f3474ffd6659923222be7355621dcc852ce7747
Y4ROSLAV <jeros1337@gmail.com>
2023-03-04 19:10:31 +0300

    Refactor code style: prettier

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,12 +1,13 @@
-
-import { ContentFooter } from './ContentParts/ContentFooter/ContentFooter';
-import { ContentHeader } from './ContentParts/ContentHeader/ContentHeader';
-import { ContentMain } from './ContentParts/ContentMain/ContentMain';
+import {ContentFooter} from './ContentParts/ContentFooter/ContentFooter'
+import {ContentHeader} from './ContentParts/ContentHeader/ContentHeader'
+import {ContentMain} from './ContentParts/ContentMain/ContentMain'
 
 export const Content = () => {
-  return <form>
-    <ContentHeader />
-    <ContentMain />
-    <ContentFooter />
-  </form>
+	return (
+		<form>
+			<ContentHeader />
+			<ContentMain />
+			<ContentFooter />
+		</form>
+	)
 }
